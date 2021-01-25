631c727e73723f79030e05b3fa5ce1f90296a4b4
Filipp Gudis <fgudis@yahoo.com>
2021-01-24 19:30:07 -0800

    shuffled array w answers working!

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,28 +1,18 @@
-import React from 'react';
-import '../App.css';
-
+import React from "react";
+import "../App.css";
 
 const button1 = (props) => {
-    
-    
-    // buttonHandler = (props) => {
-    //     console.log('method runs');
-    //     // comparison of our answer and correct answer
-    //     // somehow we have to update our counters and 
-    //   }
-     
-        return (
-            <button 
-            className='Button'
-            // onClick={this.buttonHandler}>
-            onClick={props.click}>
-                {'answer'}
-            </button>
-        );
-    }
+  // buttonHandler = (props) => {
+  //     console.log('method runs');
+  //     // comparison of our answer and correct answer
+  //     // somehow we have to update our counters and
+  //   }
 
+  return (
+    <button className="Button" onClick={props.click} color='white'>
+      {/* {"answer"} */}load questions
+    </button>
+  );
+};
 
 export default button1;
-
-
-
--- src/containers/game/game.js
+++ src/containers/game/game.js
@@ -4,98 +4,81 @@ import Scores from "../../components/scores/scores";
 import MainQ from "../../components/mainQ/mainQ";
 //import Answers from "../../components/answers/answers";
 import axios from "axios";
-import Answer from '../../components/answers/answer/answer';
-import '../../components/answers/answers.css';
-
-
+import Answer from "../../components/answers/answer/answer";
+import "../../components/answers/answers.css";
+import Button from "../../components/Button";
 class Game extends Component {
-
-
-
   state = {
-   
-    currentQuestion: "what time is it?", // displayed in question window
-    correctAnswer: '',
-    incorrectAnswers: ['1','2','3'] ,
-    fetched: false
-  }
+    currentQuestion: "Hey there! let's check how smart are you...",
+    correctAnswer: "wanna try???",
+    incorrectAnswers: [],
+    // fetched: false,
+  };
 
-  fetchData() {
-    let urlAPI = "https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple";
-    
-    axios.get(urlAPI)
+  fetchData = () => {
+    let urlAPI =
+      "https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple";
+      // 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean';
 
-      .then((response) => {
 
-        this.setState({
-          incorrectAnswers: response.data.results[0].incorrect_answers,
-          correctAnswer: response.data.results[0].correct_answer,
-          currentQuestion: response.data.results[0].question,
-         // fetched: !this.state.fetched
-                      })
-        console.log(response.data);
-      })
-      
-      .then(() => {
-        
-        console.log(this.state.currentQuestion);
-        console.log(this.state.incorrectAnswers);
-          console.log(this.state.correctAnswer);
-          // <Answer incorrectAnswers = {this.state.incorrectAnswers[0]} />
+    axios.get(urlAPI).then((response) => {
+      this.setState({
+        correctAnswer: response.data.results[0].correct_answer,
+        incorrectAnswers: response.data.results[0].incorrect_answers,
+        currentQuestion: response.data.results[0].question,
 
-        })
-  }
-  
-  componentDidMount() {
-   this.fetchData();
-   
-}
-  
+        fetched: true,
+      });
+      console.log(response.data);
+    });
+  };
 
   render() {
+    // let correctAnswer = null;
+    let options = null;
+    // let incorrectAnswers = null;
+    let answers = this.state.incorrectAnswers
+    .concat(this.state.correctAnswer)
+    .sort( () => .5 - Math.random() );
 
-    let incorrectAnswers = null;
-
-  
-
-   if (this.state.fetched) {
-
-    incorrectAnswers = <div/>;
-   }
-    else {
-   incorrectAnswers = this.state.incorrectAnswers.map( answer => {
-      
-     
-      return <Answer key = {answer} versi = {this.state.incorrectAnswers[answer]}></Answer>
-      //console.log('my render method');
-    })
-
-  }
+    // [1,2,3,4,5,6].sort( () => .5 - Math.random() );
 
 
-    // console.log(this.state.incorrect_answers);
-     
- 
 
- 
+
+
+    if (this.state.incorrectAnswers !== []) {
+
+      options = answers.map((counter) => {
+        return <Answer key={counter} versi={counter}></Answer>
+      })
+      // incorrectAnswers = this.state.incorrectAnswers.map((counter) => {
+      //   return <Answer key={counter} versi={counter}></Answer>;
+      // })
+    }
+    // correctAnswer = <Answer versi={this.state.correctAnswer}> </Answer>
+
     return (
-
-
-
       <section className="trivia-game">
         <div className="container">
           <div className="header">
             <Scores></Scores>
           </div>
 
-        <MainQ/>
-        <div className = 'answers'>
-         
-        {console.log('in my return ')}
-         {incorrectAnswers}
-
-        </div>
-
+          <MainQ currentQuestion={this.state.currentQuestion} />
+          <div className="answers">
+          
+           {options}
+            {/* {correctAnswer} */}
+            {console.log(answers)}
+            {/* <Answer versi = {this.state.correctAnswer} /> */}
+            {console.log(this.state.correctAnswer)}
+           
+            {/* {console.log("in my return ")} */}
+            {/* <Answer versi={this.state.incorrectAnswers[0]} /> */}
+            {/* <Answer c={this.state.correctAnswer} /> */}
+          </div>
+          <Button click={this.fetchData} />
           <div className="timer">timer in process ...</div>
         </div>
       </section>
